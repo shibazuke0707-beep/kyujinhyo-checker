ab7279cf1edefac97add958adea1319d71d87211
shibazuke0707-beep <shibazuke0707@gmail.com>
2026-09-09 14:02:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  * 上げ忘れると古いキャッシュが残り続ける。
  */
 
-const CACHE_VERSION = 'v4-2026-09';
+const CACHE_VERSION = 'v5-2026-09';
 const CACHE_NAME = `kyujinhyo-checker-${CACHE_VERSION}`;
 
 // オフラインでも開けるように、最初に取り込んでおくもの
